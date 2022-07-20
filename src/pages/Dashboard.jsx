3c97353e791fb1a85b7b2a1f1a1f7fbdce364265
juanibarravargas <ibs_vargas@yahoo.com>
2022-07-20 23:15:47 +0800

    sorted tasks with realtime response from the user interface

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Card, Container, Button } from "react-bootstrap";
+import { Card, Container, Button, Row, Col } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { getTask, openModal } from "../features/tasks/taskSlice";
 import { useNavigate } from "react-router-dom";
@@ -9,62 +9,74 @@ import TaskForm from "../components/TaskForm";
 import { toast } from "react-toastify";
 
 const Dashboard = () => {
-  const [completed, setCompleted] = useState([])
-  const [pending, setPending] = useState([])
+  // const [completedTasks, setCompletedTasks ] = useState([])
+  // const [pendingTasks, setPendingTasks ] = useState([])
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { tasks, isLoading, isSuccess, isError, message, toggleModal } =
-    useSelector((state) => state.task);
+  const { tasks, isLoading, isSuccess, isError, message, toggleModal }=useSelector((state) => state.task);
 
   const { user } = useSelector((state) => state.auth);
 
+
   useEffect(() => {
     if (isError) {
-      toast.error(message)
+      toast.error(message);
     }
-    
+
     if (!user) {
       return navigate("/login");
     }
     
     
-    setCompleted(tasks.filter(task =>  task.isCompleted === true ? setCompleted(completed.push(task)) : task))
-    setPending(tasks.filter(task => task.isCompleted === false ? setPending(pending.push(task)) : task))
-
     dispatch(getTask());
 
-  }, [user, navigate, isSuccess, isError, message, dispatch, setCompleted]);
+    // setCompletedTasks(tasks.filter(task => task.isCompleted))
+    // setPendingTasks(tasks.filter(task => !task.isCompleted))
+
+
+  }, [user, navigate, isSuccess, isError, message, dispatch, ]);
 
   return (
     <>
+      <Container >
+        <Card className="mb-2 mx-auto my-4 p-4">
+          <Row className="justify-content-center">
+            <div>
+              <Button variant="primary" onClick={() => dispatch(openModal())}>
+                Add Task
+              </Button>
+            </div>
+            <Col className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-10">
+              <Card className="mb-2 mx-auto my-2">
+                <Card.Header>
+                  <h3>Pending Tasks</h3>
+                </Card.Header>
+                {tasks.length > 0 ? (tasks.filter(task => {
+                  return !task.isCompleted
+                }).map((task) => (<Task key={task._id} task={task} />))
+                ) : (
+                  <h3>No Pending Tasks</h3>
+                )}
+              </Card>
+            </Col>
+            <Col className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-10">
+              <Card className="mb-2 mx-auto my-2">
+                <Card.Header>
+                  <h3>Completed Tasks</h3>
+                </Card.Header>
+                {tasks.length > 0 ? (tasks.filter(task => {
+                  return task.isCompleted
+                }).map((task) => (<CompletedTasks key={task._id} task={task} />))
+                ) : (
+                  <h3>No Pending Tasks</h3>
+                )}
+              </Card>
+            </Col>
+          </Row>
+        </Card>
+      </Container>
       <TaskForm show={toggleModal} />
-
-      <Card style={{ width: "70rem" }} className="mb-2 mx-auto my-4">
-        <div className="mx-2">
-          <Button variant="primary" onClick={() => dispatch(openModal())}>
-            Add Task
-          </Button>
-        </div>
-        <Container className="d-flex justify-content-center">
-          <Card style={{ width: "30rem" }} className="mb-2 mx-auto my-2">
-            <Card.Header>
-              <h3>Pending Tasks</h3>
-            </Card.Header>
-            {pending.length > 0 ? pending.map((task) => (
-              <Task key={task._id} task={task} />
-            )) : <h3>No Pending Tasks</h3>}
-          </Card>
-          <Card style={{ width: "30rem" }} className="mb-2 mx-auto my-2">
-            <Card.Header>
-              <h3>Completed Tasks</h3>
-            </Card.Header>
-            {completed.length > 0 ? completed.map((task) => (
-              <CompletedTasks key={task._id} task={task} />
-            )) : <h3>No Completed Tasks</h3>}
-
-          </Card>
-        </Container>
-      </Card>
     </>
   );
 };
